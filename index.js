8b6bf7d938d58fc5fa5ea6be862da3cfdf0f812b
Njalale <fnjalale@gmail.com>
2023-03-13 20:52:08 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+const currentUser = 'Njalale Fred';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
